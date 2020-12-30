4ebc076003c68fab10e06a9a2d40b0d8a561522b
Kennedy Izuegbu <kennedy@Kennedys-MacBook-Pro.local>
2020-12-30 16:24:00 +0100

    added welcome screen

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor.blue
     }
 
     @IBAction func showMessage(sender: UIButton) {
